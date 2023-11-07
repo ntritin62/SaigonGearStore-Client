041bd2378b0397bd4528e2242c9e828ab2a65738
ntritin62 <21522677@gm.uit.edu.vn>
2023-11-07 19:59:55 +0700

    finish sidebar mobile tablet

--- src/pages/ProductsPageLayout/components/Sidebar/index.js
+++ src/pages/ProductsPageLayout/components/Sidebar/index.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { useLoaderData, NavLink } from 'react-router-dom';
-const data = ['cold coffee', 'hot coffee'];
+const data = ['cold coffee', 'hot coffee', 'hot tea', 'cold tea'];
 const Sidebar = () => {
   // const NavData = useLoaderData();
   return (
-    <div className="max-w-[230px] w-full">
+    <div className="max-w-[230px] w-full md:max-w-full ">
       <h2 className="text-4xl font-bold">Categories</h2>
-      <nav className="flex flex-col gap-[16px] mt-[20px] md:flex-row">
+      <nav className="flex flex-col gap-[16px] mt-[20px] md:flex-row md:overflow-x-auto">
         {data.map((item) => (
           <NavLink
             to={`./${item}`}
             className={({ isActive }) =>
               isActive
-                ? 'flex items-center rounded-[10px] justify-between pl-[20px] font-bold h-[60px] bg-active-sidebar w-full'
-                : 'flex items-center rounded-[10px] justify-between pl-[20px] font-bold h-[60px] dark:bg-dark-sidebar bg-sidebar w-full'
+                ? 'flex items-center rounded-[10px] justify-between pl-[20px] font-bold h-[60px] flex-shrink-0 bg-active-sidebar w-full md:w-[148px] md:h-[38px] md:text-2xl md:font-medium'
+                : 'flex items-center rounded-[10px] justify-between pl-[20px] font-bold h-[60px] flex-shrink-0 dark:bg-dark-sidebar bg-sidebar w-full md:w-[148px] md:h-[38px] md:text-2xl md:font-medium'
             }
           >
             {item}
             <img
               src="/image/sidebar-img.png"
               alt=""
-              className="w-[70px] h-[60px] ob"
+              className="w-[70px] h-[60px] object-cover md:w-[44px] md:h-[30px]"
             />
           </NavLink>
         ))}
--- src/pages/ProductsPageLayout/index.js
+++ src/pages/ProductsPageLayout/index.js
@@ -4,7 +4,7 @@ import Sidebar from './components/Sidebar';
 
 const ProductsPageLayout = () => {
   return (
-    <div className="container flex gap-[30px] mt-[30px]">
+    <div className="container flex gap-[30px] mt-[30px] md:flex-col">
       <Sidebar />
       <Outlet />
     </div>
