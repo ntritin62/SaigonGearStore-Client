4cf765fb963b89bd891d7dc8454d33a58ccc083d
ntritin62 <21522677@gm.uit.edu.vn>
2023-12-07 01:27:27 +0700

    card

--- src/pages/ProfilePage/components/ProfileRight/index.js
+++ src/pages/ProfilePage/components/ProfileRight/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
+import ReactCreditCards from 'react-credit-cards-2';
+import 'react-credit-cards-2/dist/es/styles-compiled.css';
+
 const ProfileRight = () => {
-  return <section className="col-span-8">456</section>;
+  const preview = false;
+  return (
+    <section className="col-span-8 grid grid-cols-2 gap-[20px]">
+      <ReactCreditCards
+        number="371231231232111"
+        expiry="10/20"
+        cvc="235"
+        name="JOHN SMITH"
+      />
+      <ReactCreditCards
+        number="491231231232111"
+        expiry="10/20"
+        cvc="235"
+        name="JOHN SMITH"
+      />
+      <ReactCreditCards
+        number="511231231232111"
+        expiry="10/20"
+        cvc="235"
+        name="JOHN SMITH"
+      />
+    </section>
+  );
 };
 
 export default ProfileRight;
--- src/pages/ProfilePage/index.js
+++ src/pages/ProfilePage/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ProfileLeft from './components/ProfileLeft';
 import ProfileRight from './components/ProfileRight';
-import { Outlet } from 'react-router-dom';
+// import { Outlet } from 'react-router-dom';
 
 const ProfilePage = () => {
   return (
-    <main className=" container grid grid-cols-11 mt-[30px] ">
+    <main className=" container grid grid-cols-11 mt-[30px] md:flex md:flex-col">
       <ProfileLeft />
-      <Outlet />
+      <ProfileRight />
     </main>
   );
 };
