d955904b31de169e0ef68472df99790f8439bdea
ntritin62 <21522677@gm.uit.edu.vn>
2024-03-15 11:33:48 +0700

    finished top left payment method page

--- src/pages/PaymentPage/index.js
+++ src/pages/PaymentPage/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import * as ROUTES from '../../constants/routes';
+import { Link } from 'react-router-dom';
 const PaymentPage = () => {
   return (
     <>
@@ -16,7 +17,46 @@ const PaymentPage = () => {
       </div>
       <div className="container grid grid-cols-11 xl:flex xl:flex-col my-[30px] gap-[30px] dark:text-checkout-text">
         <div className="col-span-8">
-          <div className="p-30px"></div>
+          <div className="p-[30px] rounded-[20px] bg-white dark:bg-dark-sidebar">
+            <div className="flex items-center justify-between">
+              <h2 className="text-3xl font-medium">
+                1. Shipping, arrives between Mon, May 16—Tue, May 24
+              </h2>
+              <Link to={ROUTES.SHIPPING}>
+                <button className="flex gap-[10px] mt-auto mr-[15px]">
+                  <img src="/icon/edit.svg" alt="" className="dark-icon" />
+                  <span>Edit</span>
+                </button>
+              </Link>
+            </div>
+            <div className="my-[30px] bg-body-bg dark:bg-dark-body-bg p-[20px] rounded-[20px] flex justify-between items-center">
+              <div>
+                <p className="text-2xl font-medium mb-[4px]">Imran Khan</p>
+                <p>Museum of Rajas, Sylhet Sadar, Sylhet 3100.</p>
+              </div>
+              <div className="w-[31px] h-[31px]">
+                <img src="/icon/check.svg" alt="" className="w-full h-full" />
+              </div>
+            </div>
+            <div className="my-[30px] bg-body-bg dark:bg-dark-body-bg p-[20px] rounded-[20px] flex justify-between items-center">
+              <div>
+                <p className="text-2xl font-medium mb-[4px]">Items details</p>
+                <p>2 items</p>
+              </div>
+              <Link
+                to={ROUTES.SHIPPING}
+                className="underline underline-offset-5"
+              >
+                View details
+              </Link>
+            </div>
+          </div>
+          <div className="p-[30px] rounded-[20px] bg-white dark:bg-dark-sidebar mt-[30px]">
+            <div className="flex items-center justify-between">
+              <h2 className="text-3xl font-medium">2. Shipping method</h2>
+            </div>
+            <div className="w-full h-[1px] my-[30px] bg-top-menu-border dark-bg-dark-body-bg"></div>
+          </div>
         </div>
         <div className="col-span-3">456</div>
       </div>
